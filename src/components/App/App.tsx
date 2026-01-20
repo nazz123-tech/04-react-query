cf3ab0512ee041be31002f90dfcc5642c9388fb6
nazar <road2thedream1@gmail.com>
2026-01-20 10:14:07 +0100

    fixed toast

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import css from './App.module.css'
 import 'modern-normalize';
 import SearchBar from '../SearchBar/SearchBar';
@@ -41,7 +41,11 @@ export default function App() {
 const totalPages=data?.total_pages ?? 0;
  const movies: Movie[] = data?.results ?? [];
  const noMovies = movies.length == 0;
- 
+ useEffect(()=>{
+    if(noMovies && isSuccess){
+      toast.error("No movies found for your request.")
+    }
+ }, [noMovies, isSuccess])
   return (
     <> 
       <SearchBar onSubmit={handleSearch}/>
@@ -58,7 +62,6 @@ const totalPages=data?.total_pages ?? 0;
         previousLabel="←"
         />
       )}
-      {isSuccess && noMovies && toast.error("No movies found for your request.")}
       {isLoading && <Loader/>}
       {isError && <ErrorMessage />}
       {!isError && movies.length > 0 && (
